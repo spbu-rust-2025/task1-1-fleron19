73a797261f37c7e627b8d54927f878838198097a
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-11-03 00:01:38 +0300

    Add first task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut input = String::new(); 
+    let sum;
+    let _ = io::stdin().read_line(&mut input);
+    let mut parts = input.split_whitespace().map(|input| input.parse::<i32>());
+    match (parts.next(), parts.next()) {
+        (Some(Ok(a)), Some(Ok(b))) => {
+            sum = a + b;
+            println!("{}", sum);
+        }
+        _ => {} 
+    }
 }
